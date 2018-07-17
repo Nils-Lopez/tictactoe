26e574ddae4c790056f67e4a029e355ce6d13e04
Hugo Payet <hugopayetsync@gmail.com>
2018-07-17 23:20:07 +0200

    ajout commentaires

--- programme/board.rb
+++ programme/board.rb
@@ -4,13 +4,13 @@ class Board
     # Quand la classe s'initialize, elle doit créer 9 instances BoardCases
     # Ces instances sont rangées dans une array qui est l'attr_accessor de la classe
     @boardcases = []
-    for i in (1..9)
+    for i in (1..9)   #crée une array de 9 cases
       @boardcases << BoardCase.new(i.to_s)
     end
     print_board
   end
 
-  def print_board
+  def print_board  #affiche une magnifique board pimpée avec colorize
     barre = '|'.green
     x = ' ' * 3
     y = ' ' * 14 + barre + ' ' * 7 + barre
@@ -36,10 +36,11 @@ class Board
 
 
   def play(choice, result)
-     # Change la BoardCase jouée en fonction de la valeur du joueur (X, ou O)
+  ##on applique sur la board le choix de l'utilisateur renvoyé par le fichier game.rb, choice - 1 est utilisé pour trouver l'index de la case qui commence à 0.
     @boardcases[choice - 1].value = result
   end
 
+  #cette méthode est utilisée dans game.rb pour déterminer si la case peut être jouée. 
   def already_played?(choice)
     if @boardcases[choice - 1].value == 'X'.blue.bold || @boardcases[choice - 1].value == 'O'.red.bold
       true
@@ -49,7 +50,7 @@ class Board
   end
 
   def victory?
-    # Qui gagne ?
+  #recherche si une condition de victoire est satisfaite, le résultat est utilisé dans la méthode launch_game de game.rb
     win_combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     victory = 0
     win_combos.each do |combo|
--- programme/game.rb
+++ programme/game.rb
@@ -11,7 +11,8 @@ class Game
      ║ ║║      ║ ╠═╣║      ║ ║ ║║╣   │ │
      ╩ ╩╚═╝    ╩ ╩ ╩╚═╝    ╩ ╚═╝╚═╝  o o
     \n\n"
-
+#créé le player1, une nouvelle instance de le classe Player, avec un nom et un symbole. 
+#Le '.blue.bold' est une méthode de la gem colorize
     puts 'Quel est le nom du premier joueur ?'
     print '> '
     player1_firstname = gets.chomp
@@ -29,7 +30,12 @@ class Game
 
   def launch_game
     # Lance la partie
+    #il y a 9 tours maximum, sinon il y a une égalité
     9.times do |i|
+      #appelle @board.victory pour déterminer avant chaque tour si une condition 
+      #de victoire est remplie? Si le résultat est non, on lance le prochain tour, 
+      #si le résultat est oui, on retrouve le nom du joueur qui a gagné pour l'afficher
+
       if @board.victory? == 0
         turn(i)
       else
@@ -58,11 +64,12 @@ class Game
   end
 
   def turn(i)
-    # Affiche le plateau, demande au joueur il joue quoi, passe au joueur suivant si la partie n'est pas finie
+    #à chaque tour, le joueur change. A chaque itération, i est incrémenté de 1, n est pair ou impair, n = i % 2 vaut 0 ou 1
     n = i % 2
     puts "#{@players[n].firstname}, sur quelle case souhaites-tu jouer (entre 1 et 9) ?"
     choice = gets.chomp.to_i
-
+#on regarde si la valeur donnée par l'utilisateur est valide, si elle ne l'est pas, une boucle s'ouvre pour afficher la cause 
+#de l'invalidité et réafficher le formulaire. La boucle ne se ferme que quand les deux conditions sont respéctées
     while !choice.between?(1, 9) || (@board.already_played?(choice) == true)
       if !choice.between?(1, 9)
         puts "Tu dois saisir un nombre entre 1 et 9 ! #{@players[n].firstname}, sur quelle case souhaites-tu vraiment jouer ?"
@@ -73,7 +80,7 @@ class Game
         choice = gets.chomp.to_i
       end
     end
-
+#une fois que les conditions de validité sont respéctées, on crée l'action demandée (quel symbole sur quelle case) et on va afficher le résultat sur la board
     @board.play(choice, @players[n].type)
     @board.print_board
   end
